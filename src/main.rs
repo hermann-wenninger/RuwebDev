41847c64f2e5c72d79e66d628bd79ed5545d7d5c
wennhmann <info@wennhmann.de>
2025-05-20 17:14:02 +0200

    position person

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+enum Position{
+    SuperUser(Person)
+    Artist(Person)
+    User(Person)
+}
+struct Person{
+    name:String,
+    passwort:String,
+
+}
+
 fn main() {
     println!("Hello, world!");
 }
